79de324a661162787904d812c351d98614d6b879
EC2 Default User <ec2-user@ip-172-31-3-245.ap-northeast-1.compute.internal>
2023-04-01 07:40:46 +0000

    [Update] books showページ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,28 +1,28 @@
 class BooksController < ApplicationController
-  
+
   def index
     @user = current_user
     @book = Book.new
     @books = Book.all
   end
-  
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
     redirect_to book_path(@book.id)
   end
-  
+
   def show
     @user = current_user
     @book = Book.new
     @book = Book.find(params[:id])
   end
-  
+
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
@@ -35,5 +35,5 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
