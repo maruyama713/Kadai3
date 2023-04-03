fb48ccf8b7fbac48913b6ab73e4f0eb6b17e04c4
EC2 Default User <ec2-user@ip-172-31-3-245.ap-northeast-1.compute.internal>
2023-04-03 06:55:56 +0000

    [Add] user,edit エラーメッセージ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  
+  def create
+  end
 
   def show
     @user = User.find(params[:id])
@@ -9,4 +12,21 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:hoge] = "You have updated user successfully."
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+  
+   private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction)
+  end
+
 end
